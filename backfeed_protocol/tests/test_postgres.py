852c54b82f16033c3ca96ec1de933789516172de
Jelle Gerbrandy <github@gerbrandy.com>
2016-06-08 10:58:41 +0200

    add explanatino to set up postgres test

--- backfeed_protocol/tests/test_postgres.py
+++ backfeed_protocol/tests/test_postgres.py
@@ -12,7 +12,7 @@ class TestWithPostgres(BaseContractTestCase):
     """All the other tests run in memory - here we test with files"""
 
     settings = {
-        'sqlalchemy.url': 'postgresql://backfeed-test:backfeed@localhost:5432/backfeed-test',
+        'sqlalchemy.url': 'postgresql://backfeed_test:backfeed@localhost:5432/backfeed_test',
     }
 
     def setUp(self):
@@ -21,21 +21,17 @@ class TestWithPostgres(BaseContractTestCase):
             super(TestWithPostgres, self).setUp()
             self.db_ok = True
         except sqlalchemy.exc.OperationalError as error:
-            msg = ''
-            msg += '\n'
-            msg += '*' * 80
-            msg += '\n'
-            msg += "WARNING: There was an error connecting to the postgres server."
-            msg += '\n'
-            msg += "For this test to work, you need to set up a test database"
-            msg += '\n'
-            msg += self.settings['sqlalchemy.url']
-            msg += '\n'
-            msg += 'See the CONTRIBUTING file for details'
-            msg += '\n'
-            msg += unicode(error)
-            msg += '\n'
-            msg += '*' * 80
+            msg = []
+            msg.append('*' * 80)
+            msg.append("WARNING: There was an error connecting to the postgres server.")
+            msg.append("For this test to work, you need to set up a test database")
+            msg.append(self.settings['sqlalchemy.url'])
+            msg.append('psql -c "create database backfeed_test"')
+            msg.append('psql -c "create user backfeed_test password \'backfeed\'"')
+            msg.append('psql -c "grant all on database backfeed_test to backfeed_test"')
+            msg.append(unicode(error))
+            msg.append('*' * 80)
+            msg = '\n'.join(msg)
             self.db_ok = False
             logging.error(msg)
 
